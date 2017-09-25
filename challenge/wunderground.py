9b37e0191fe75d5ce5ffb1e288d21034c1e09ab3
Glen Haber <GHaber@BehaviorMatrix.com>
2017-09-25 19:58:22 -0400

    Wrap API call in function for reusability

--- challenge/wunderground.py
+++ challenge/wunderground.py
@@ -1,16 +1,24 @@
 import requests
 
-site = 'http://api.wunderground.com/api/'
-endpoint = '/conditions/q/PA/Philadelphia.json'
-key = '4b787f11700ab380'
 
-page = site + key + endpoint
+def print_weather(city, state):
+    city = city.title().replace(' ', '_')
+    state = state.upper()
+    site = 'http://api.wunderground.com/api/'
+    endpoint = '/conditions/q/{}/{}.json'.format(state, city)
+    key = '4b787f11700ab380'
 
-response = requests.get(page)
-weather = response.json()
+    page = site + key + endpoint
 
-temperature = weather['current_observation']['temp_f']
-conditions = weather['current_observation']['weather']
+    response = requests.get(page)
+    weather = response.json()
 
-print('Weather in Philadelphia:')
-print('{}, {}°F'.format(conditions, temperature))
+    temperature = weather['current_observation']['temp_f']
+    conditions = weather['current_observation']['weather']
+
+    print('Weather in {}:'.format(city))
+    print('{}, {}°F'.format(conditions, temperature))
+
+
+if __name__ == '__main__':
+    print_weather('philadelphia', 'pa')
