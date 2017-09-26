ad839acb7a99c6a332f4fefddfc9eb568a3db39a
Glen Haber <GHaber@BehaviorMatrix.com>
2017-09-25 20:22:03 -0400

    Add docstrings

--- challenge/wunderground.py
+++ challenge/wunderground.py
@@ -1,3 +1,11 @@
+"""
+Description of code challenge:
+
+Write a Python script that uses a module, that you create, to interface with the
+Weather Underground API (https://www.wunderground.com/weather/api) to query the
+current temperature and conditions in Philadelphia, PA. The results should be
+printed to the screen as text. The module should be reusable.
+"""
 import requests
 
 with open('api_key.cfg') as f:
@@ -5,6 +13,11 @@ with open('api_key.cfg') as f:
 
 
 def print_weather(city, state):
+    """ Print the temperature and current conditions for the given city
+
+    :param city: the full name of the city to check
+    :param state: the 2-letter abbreviation for the state that the city is in
+    """
     city = city.title().replace(' ', '_')
     state = state.upper()
     site = 'http://api.wunderground.com/api/'
